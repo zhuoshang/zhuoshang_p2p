28c908c81666556dfa352b7235def89cb495cf00
tianling <2507073658@qq.com>
2015-04-08 10:54:28 +0800

    pic

---
+++ app/DebtPic.php
@@ -0,0 +1,15 @@
+<?php namespace App;
+/*
+**Author:tianling
+**createTime:15/4/8 上午10:52
+*/
+
+use Illuminate\Database\Eloquent\Model;
+
+class DebtPic extends Model{
+
+    protected $table = 'debtPic';
+
+    public $timestamps = false;
+}
+
